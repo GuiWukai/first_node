5630ecdd58831c124bb354157ddc8055bcf6029e
Guillaume Ah-ki <guillaume_ahki@hotmail.com>
2017-05-31 18:57:50 -0400

    added demohttp

--- myfirst.js
+++ myfirst.js
@@ -1,7 +1,7 @@
 var http = require('http'); //module to create server
 var dt = require('./myfirstmodule');  //./ means current directory
 
-http.createServer(function (req, res) {
+http.createServer(function (req, res) {    //request and response
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("The date and time are currently: " + dt.myDateTime());
     res.end();
