6d9b70c0dedd7972ec2108090a1ffd4d2f2d7566
Guillaume Ah-ki <guillaume_ahki@hotmail.com>
2017-05-31 19:32:47 -0400

    added multiple files

--- demo_readfile.js
+++ demo_readfile.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var fs = require('fs');
+var fs = require('fs');  //module file system
 http.createServer(function (req, res) {
   fs.readFile('demofile1.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
